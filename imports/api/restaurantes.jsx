fb5338f67bd8f047c65c593c3a443b9094db6e3d
necabrera <ne.cabrera@uniandes.edu.co>
2018-04-09 17:12:00 -0500

    correccion

--- imports/api/restaurantes.jsx
+++ imports/api/restaurantes.jsx
@@ -6,47 +6,47 @@ export const Restaurantes = new Mongo.Collection("restaurantes");
 
 
 if(Meteor.isServer) {
-  Meteor.publish("restaurantes", function restaurantesPublication() {
-    return Restaurantes.find();
-  });
+    Meteor.publish("restaurantes", function restaurantesPublication() {
+        return Restaurantes.find();
+    });
 }
 
 Meteor.methods({
-  "restaurantes.insert"(name, imgUrl, stadiumT) {
-    check(name, String);
-    check(imgUrl, String);
-    userRole = Meteor.users.findOne(this.userId).profile.role;
-    if(!this.userId || userRole !== "restaurant") {
-      throw new Meteor.Error("not-authorized");
-    }
+    "restaurantes.insert"(name, imgUrl, stadiumT) {
+        check(name, String);
+        check(imgUrl, String);
+        userRole = Meteor.users.findOne(this.userId).profile.role;
+        if(!this.userId || userRole !== "restaurant") {
+            throw new Meteor.Error("not-authorized");
+        }
 
-    Restaurantes.insert({
-      name: name,
-      img: imgUrl,
-      owner: this.userId,
-      stadium: stadiumT,
-      menu: [{}]
-    });
+        Restaurantes.insert({
+            name: name,
+            img: imgUrl,
+            owner: this.userId,
+            stadium: stadiumT,
+            menu: []
+        });
 
-  },
-  "restaurantes.insertMenu"(plate, restaurantId) {
-    check(plate, Object);
-    check(restaurantId, String);
-    // restaurant = Meteor.Restaurantes.findOne(restaurantId);
-    //if(restaurant.owner !== userId) {
-    //throw new Meteor.Error("not-Authorized");
-    //}
-    Restaurantes.update(restaurantId, {
-      $push: { menu: plate }
-    });
-  },
-  "restaurantes.uploadPic"(pic) {
-    check(pic, String);
-    userRole = Meteor.users.findOne(this.userId).profile.role;
-    if(!this.userId || userRole !== "restaurant") {
-      throw new Meteor.Error("not-authorized");
+    },
+    "restaurantes.insertMenu"(plate, restaurantId) {
+        check(plate, Object);
+        check(restaurantId, String);
+        // restaurant = Meteor.Restaurantes.findOne(restaurantId);
+        //if(restaurant.owner !== userId) {
+        //throw new Meteor.Error("not-Authorized");
+        //}
+        Restaurantes.update(restaurantId, {
+            $push: { menu: plate }
+        });
+    },
+    "restaurantes.uploadPic"(pic) {
+        check(pic, String);
+        userRole = Meteor.users.findOne(this.userId).profile.role;
+        if(!this.userId || userRole !== "restaurant") {
+            throw new Meteor.Error("not-authorized");
+        }
+        Restaurantes.update({ owner: this.userId }, { $set: { img: pic } });
     }
-    Restaurantes.update({ owner: this.userId }, { $set: { img: pic } });
-  }
 });
 
